f05496c89945ea65ed6202642dc563fa4366c89e
owenford1209 <owen.anderson@creospan.com>
2025-07-28 15:06:29 -0500

    CORS

--- backend/server.py
+++ backend/server.py
@@ -442,7 +442,10 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://frontend-fx0y.onrender.com",
+        "http://localhost:3000"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
